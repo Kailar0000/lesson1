d8acfad34f683c91069ea8cbeda414f1bfefdd07
Kailar0000 <kailar0000@gmail.com>
2022-11-29 17:58:01 +0300

    Task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,5 +3,10 @@ Console.Write("Введите 1-ое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if(a>b)
-Console.WriteLine()
+
+if (a>b) 
+    Console.WriteLine($"1-ое число {a} больше 2-ого числа {b}");
+else if (a<b) 
+    Console.WriteLine($"2-ое число {b} больше 1-ого числа {a}");
+else
+    Console.WriteLine($"1-ое число {a} равно 2-му числу {b}");
